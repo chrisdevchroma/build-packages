8087d0e3ae1d0dcf47ae87e03a51be62155cf4e9
chrisdevchroma <chris10ros@gmail.com>
2020-02-16 18:47:48 +0100

    Removed obselete packages

--- deluge.sh
+++ deluge.sh
@@ -2,23 +2,11 @@
 . ./shared_functions.sh
 
 # Install deluge build dependencies
-pkg_install python3-devel python3-wheel
+pkg_install python3-devel python3-wheel libappstream-glib desktop-file-utils
 # Build & install dependency rb_libtorrent & rb_libtorrent-python3 -> see https://github.com/chrisdevchroma/rb_libtorrent-rhel
-./rb_libtorrent.sh
-pkg_install "${PKGS_DIR}/rb_libtorrent-[:digit:]*.rpm"
-pkg_install "${PKGS_DIR}/rb_libtorrent-python3-*.rpm"
-# Build & install dependency python3-setproctitle -> see https://github.com/chrisdevchroma/python-setproctitle-rhel
-./python-setproctitle.sh
-pkg_install "${PKGS_DIR}/python3-setproctitle-*.rpm"
-# Build & install dependency python3-GeoIP -> see https://github.com/chrisdevchroma/python-GeoIP-rhel
-./python-GeoIP.sh
-pkg_install "${PKGS_DIR}/python3-GeoIP-*.rpm"
-# Build & install dependency python3-pygame -> see https://github.com/chrisdevchroma/pygame-rhel
-./pygame.sh
-pkg_install "${PKGS_DIR}/python3-pygame-*.rpm"
-# Build & install dependency python3-rencode -> see https://github.com/chrisdevchroma/python-rencode-rhel
-./python-rencode.sh
-pkg_install "${PKGS_DIR}/python3-rencode-*.rpm"
+#./rb_libtorrent.sh
+pkg_install "${PKGS_DIR}/rb_libtorrent-1.*.x86_64.rpm"
+pkg_install "${PKGS_DIR}/rb_libtorrent-python3-1.*.x86_64.rpm"
 # Clone repo with git and cd into the folder
 clone_repo https://github.com/chrisdevchroma/deluge-rhel.git
 pushd deluge-rhel
--- rb_libtorrent.sh
+++ rb_libtorrent.sh
@@ -5,11 +5,7 @@
 pkg_install boost-devel boost-python3-devel chrpath python3-devel
 # Build & install dependency asio-devel -> see https://github.com/chrisdevchroma/asio-rhel
 ./asio.sh
-pkg_install "${PKGS_DIR}/asio-devel*.rpm"
-# Build & install dependency libtommath & libtommath-devel -> see https://github.com/chrisdevchroma/libtommath-rhel
-./libtommath.sh
-pkg_install "${PKGS_DIR}/libtommath-[:digit:]*.rpm"
-pkg_install "${PKGS_DIR}/libtommath-devel-*.rpm"
+pkg_install "${PKGS_DIR}/asio-devel-*.x86_64.rpm"
 # Clone repo with git and cd into the folder
 clone_repo https://github.com/chrisdevchroma/rb_libtorrent-rhel.git
 pushd rb_libtorrent-rhel
