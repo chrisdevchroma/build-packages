f0cc6447510adfbd074e75ab8934981ca91e9aad
chrisdevchroma <chris10ros@gmail.com>
2020-04-05 09:21:36 +0200

    Enabled rb_libtorrent again

--- deluge.sh
+++ deluge.sh
@@ -4,7 +4,7 @@
 # Install deluge build dependencies
 pkg_install python3-devel python3-wheel libappstream-glib desktop-file-utils
 # Build & install dependency rb_libtorrent & rb_libtorrent-python3 -> see https://github.com/chrisdevchroma/rb_libtorrent-rhel
-#./rb_libtorrent.sh
+./rb_libtorrent.sh
 pkg_install "./${PKGS_DIR}/rb_libtorrent-[[:digit:]]*.x86_64.rpm"
 pkg_install "./${PKGS_DIR}/rb_libtorrent-python3-[[:digit:]]*.x86_64.rpm"
 # Clone repo with git and cd into the folder
